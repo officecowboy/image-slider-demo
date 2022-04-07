857be6b5c115ef3876e1e020918e23da51895cad
Scott Diekema <scott.diekema@gmail.com>
2022-04-07 16:27:57 -0400

    successful fetch + display

--- image-slider-app/src/App.js
+++ image-slider-app/src/App.js
@@ -1,9 +1,42 @@
 import "./App.css";
-import testImage from "./assets/test-photo.jpg";
+import React, { useState } from "react";
 import { ReactComponent as BackArrow } from "./assets/arrow-left.svg";
 import { ReactComponent as NextArrow } from "./assets/arrow-right.svg";
+let url = "https://api.unsplash.com/topics/experimental/photos/?client_id=hjI8EnFur9yTS8YMMiJ5lKfyY5vS21n3OMguS1EGxew"
+
+const images = []
+
+fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    images.push(data.map(image => image.urls.regular))
+  })
+  .catch(err => {
+    console.log('caught it!', err);
+  });
+
+console.log(images)
 
 function App() {
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const handleBack = () => {
+    if (currentImage === 0) {
+      setCurrentImage(images.length - 1);
+    } else {
+      setCurrentImage((currentImage) => currentImage - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentImage === images.length - 1) {
+      setCurrentImage(0);
+    } else {
+      setCurrentImage((currentImage) => currentImage + 1);
+    }
+  };
+
   return (
     <div className="App">
       <header>
@@ -15,12 +48,12 @@ function App() {
 
       <figure>
         <nav>
-          <BackArrow />
-          <NextArrow />
+          <BackArrow onClick={handleBack} />
+          <NextArrow onClick={handleNext} />
         </nav>
         <figcaption>
         </figcaption>
-        <img src={testImage} alt=""></img>
+        <img alt="" src={images[currentImage]} />
       </figure>
 
     </div>
