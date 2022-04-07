42699b7daf8d7102a42732dc478ba3abe6362aea
Scott Diekema <scott.diekema@gmail.com>
2022-04-07 15:01:53 -0400

    app.js html structure

--- image-slider-app/src/App.js
+++ image-slider-app/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Digital Images</h1>
+        <h2>
+          Gallery created with React.js and the Unsplash API
+        </h2>
       </header>
+
+      <figure>
+        <nav>
+        </nav>
+        <figcaption>
+        </figcaption>
+        <img src="" alt=""></img>
+      </figure>
+
     </div>
   );
 }
